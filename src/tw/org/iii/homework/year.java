a8aa1c0829b84a48563bb1eb75cfc9cf2b050931
Stan12000 <ra72636@gmail.com>
2016-08-16 20:15:38 +0800

    ONE

--- src/tw/org/iii/homework/year.java
+++ src/tw/org/iii/homework/year.java
@@ -8,28 +8,15 @@ public class year {
 	public static void main(String[] args) throws IOException { 
 		
 		int year,i;
-		
 		for (i=1;i>=0;i++){
-		System.out.println("糗輸入年粉ˋ:");
-		
-		
-		
-		
-		Scanner scanner = new Scanner(System.in);
-		year = scanner.nextInt();
-		
-		
-
-	
-		if(year%400 == 0 ){System.out.println("閏年");}
-		else if(year%100 == 0){System.out.println("不是閏年");}
-		else if(year%4 == 0){System.out.println("閏年");}
-		else{System.out.println("不是閏年");}
-		
-		
-		
+		 System.out.print("糗輸入年粉:");
+		 Scanner scanner = new Scanner(System.in);
+		 year = scanner.nextInt();
+		 if(year%400 == 0 ){System.out.println("閏年");}
+		  else if(year%100 == 0){System.out.println("不是閏年");}
+		  else if(year%4 == 0){System.out.println("閏年");}
+		  else{System.out.println("不是閏年");}
 		i = year;
-		
 		if(i == 0) break;
 		
 		}
