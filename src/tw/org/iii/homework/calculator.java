bcaf086549f6649069975b35551d0918aac5aa48
Stan12000 <ra72636@gmail.com>
2016-09-01 21:08:37 +0800

    20160901

--- src/tw/org/iii/homework/calculator.java
+++ src/tw/org/iii/homework/calculator.java
@@ -6,17 +6,22 @@ import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.sun.xml.internal.ws.org.objectweb.asm.Label;
+
 public class calculator extends JFrame implements ActionListener{
 
-	
+	private Object tmp;
+	 JLabel label;
+	 JButton c,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16;
+	 
 	public calculator(){
 		
 		super("§p∫‚ΩL");
@@ -24,29 +29,30 @@ public class calculator extends JFrame implements ActionListener{
 	    setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		JLabel label = new JLabel("	≈„•‹");
+		 label = new JLabel("123");
 		JPanel top = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		JPanel center =new JPanel();
+		
 		top.add(label);
 		
 
 		center.setLayout(new GridLayout(4,4));
-		  Button a1 = new  Button("7");
-		  Button a2 = new  Button("8");
-		  Button a3 = new  Button("9");
-		  Button a4 = new  Button("/");
-		  Button a5 = new  Button("4");
-		  Button a6 = new  Button("5");
-		  Button a7 = new  Button("6");
-		  Button a8 = new  Button("X");
-		  Button a9 = new  Button("1");
-		  Button a10 = new  Button("2");
-		  Button a11 = new  Button("3");
-		  Button a12 = new  Button("-");
-		  Button a13 = new  Button("0");
-		  Button a14 = new  Button(".");
-		  Button a15 = new  Button("=");
-		  Button a16 = new  Button("+");
+		   a1 = new  JButton("7"); 
+		   a2 = new  JButton("8"); 
+		   a3 = new  JButton("9");
+		   a4 = new  JButton("/");
+		   a5 = new  JButton("4");
+		   a6 = new  JButton("5");
+		   a7 = new  JButton("6");
+		   a8 = new  JButton("X");
+		   a9 = new  JButton("1");
+		   a10 = new  JButton("2");
+		   a11 = new  JButton("3");
+		   a12 = new  JButton("-");
+		   a13 = new  JButton("0");
+		   a14 = new  JButton(".");
+		   a15 = new  JButton("=");
+		   a16 = new  JButton("+");
 		  center.add(a1);
 		  center.add(a2);
 		  center.add(a3);
@@ -63,8 +69,22 @@ public class calculator extends JFrame implements ActionListener{
 		  center.add(a14);
 		  center.add(a15);
 		  center.add(a16);
-		  a1.addActionListener(new Listener());
-		  
+		  a1.addActionListener(this);
+		  a2.addActionListener(this);
+		  a3.addActionListener(this);
+		  a4.addActionListener(this);
+		  a5.addActionListener(this);
+		  a6.addActionListener(this);
+		  a7.addActionListener(this);
+		  a8.addActionListener(this);
+		  a9.addActionListener(this);
+		  a10.addActionListener(this);
+		  a11.addActionListener(this);
+		  a12.addActionListener(this);
+		  a13.addActionListener(this);
+		  a14.addActionListener(this);
+		  a15.addActionListener(this);
+		  a16.addActionListener(this);
 		  
 
 		setLayout(new BorderLayout());
@@ -72,10 +92,10 @@ public class calculator extends JFrame implements ActionListener{
 		add(center,BorderLayout.CENTER);
 
 
-		
+	  
 	
-		
-	}
+	}	
+	
 	
 	
 	
@@ -84,8 +104,10 @@ public class calculator extends JFrame implements ActionListener{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       new calculator();
+	}  
       
-	}
+      
+	
 
 
 
@@ -94,20 +116,13 @@ public class calculator extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+     
+     JButton c= (JButton)e.getSource();
+	 //label.setText(c.getText());
 
-		 String b = "7";
-
-		
-	}
-
- }
-
- class Listener implements ActionListener{
+	} 
 	 
-	 public void actionPerformed(ActionEvent ee){
-		 String b = "7";
-		 
-	 }
 	 
-	 }
- 
+}
+	 
+	
